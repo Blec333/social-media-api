3a2569885d5cf4ed380d17a5e3acb8159d690396
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-20 21:57:47 -0700

    create reaction.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,22 +1,20 @@
 const { Schema, Types } = require('mongoose');
 
-const assignmentSchema = new Schema(
+const reactionSchema = new Schema(
   {
-    assignmentId: {
+    reactionId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId(),
     },
-    assignmentName: {
+    username: {
       type: String,
       required: true,
-      maxlength: 50,
-      minlength: 4,
-      default: 'Unnamed assignment',
     },
-    score: {
-      type: Number,
+    reactionName: {
+      type: String,
       required: true,
-      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+      maxlength: 280,
+      default: 'Unnamed reaction',
     },
     createdAt: {
       type: Date,
@@ -34,4 +32,4 @@ const assignmentSchema = new Schema(
 
 
 
-module.exports = assignmentSchema;
+module.exports = reactionSchema;
